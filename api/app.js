2763682a21f79d211ed002c71e90e086d77e58b5
Douglas Uretsky <douglasuretsky@gmail.com>
2019-09-09 19:19:42 -0400

    update express routes for business search and reviews search to yelp api

--- api/app.js
+++ api/app.js
@@ -1,63 +1,76 @@
-require('dotenv').config()
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
+// require('dotenv').config()
+// var createError = require('http-errors');
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+// var cors = require('cors');
 
-const Yelp = require('./modules/Yelp')
-const yelp = require('yelp-fusion');
-const yelpClient = yelp.client(process.env.YELP_API_KEY);
+// const Yelp = require('./modules/Yelp')
+// const yelp = require('yelp-fusion');
+// const yelpClient = yelp.client(process.env.YELP_API_KEY);
 
-const Zomato =  require('./modules/Zomato')
-const zomato = require('zomato-api');
-const zomatoClient = zomato({
-  userKey: process.env.ZOMATO_API_KEY
-})
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
-
-app.use(function(req, res, next) {
-  const yelpAPI = new Yelp(yelpClient, null, null)
-  yelpAPI.searchTerm = req.body.term;
-  yelpAPI.searchLocation = req.body.location;
-  yelpAPI.search(req, res, next)
-})
-
-// app.use(function(req, res, next) {
-//   let zomatoAPI = new Zomato(zomatoClient, null, null)
-//   zomatoAPI.searchTerm = req.body.term;
-//   zomatoAPI.searchLocation = req.body.location;
-//   zomatoAPI.search(req, res, next)
+// const Zomato =  require('./modules/Zomato')
+// const zomato = require('zomato-api');
+// const zomatoClient = zomato({
+//   userKey: process.env.ZOMATO_API_KEY
 // })
 
-app.use(function(req, res, next) {
-  let finalResult = {
-    search: req.body,
-    yelpData: req.yelpData,
-    // zomatoData: req.zomatoData
-  }
-  res.send(finalResult)
-})
+// var app = express();
 
-app.post(`http://localhost:9000/businesses/:id`, function (req, res, next) {
-  yelpClient.business('gary-danko-san-francisco').then(response => {
-    res.send('hi');
-  }).catch(e => {
-    console.log(e);
-  });
-})
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(cors());
+
+// app.use(function(req, res, next) {
+//   const yelpAPI = new Yelp(yelpClient, null, null)
+//   yelpAPI.searchTerm = req.body.term;
+//   yelpAPI.searchLocation = req.body.location;
+//   yelpAPI.search(req, res, next)
+// })
+
+// // app.use(function(req, res, next) {
+// //   let zomatoAPI = new Zomato(zomatoClient, null, null)
+// //   zomatoAPI.searchTerm = req.body.term;
+// //   zomatoAPI.searchLocation = req.body.location;
+// //   zomatoAPI.search(req, res, next)
+// // })
+
+// app.use(function(req, res, next) {
+//   let finalResult = {
+//     search: req.body,
+//     yelpData: req.yelpData,
+//     // zomatoData: req.zomatoData
+//   }
+//   res.send(finalResult)
+// })
+
+// app.post(`http://localhost:9000/businesses/:id`, function (req, res, next) {
+//   yelpClient.business('gary-danko-san-francisco').then(response => {
+//     res.send('hi');
+//   }).catch(e => {
+//     console.log(e);
+//   });
+// })
+
+// module.exports = app;
+require('dotenv').config()
+var express = require('express');
+var app = express();
+var cors = require('cors');
+var router = express.Router();
+const yelp = require('yelp-fusion');
+const yelpClient = yelp.client(process.env.YELP_API_KEY);
+app.use(cors())
+app.get("/",function(req,res){
+  // res.json({"message" : "Hello World"});
+  console.log('hi')
+});
 module.exports = app;
--- client/src/actions/BusinessActions.js
+++ client/src/actions/BusinessActions.js
@@ -1,5 +1,55 @@
 import * as types from './ActionTypes'
 
+// const setBusinesses = payload => {
+//     return {
+//         type: types.GET_BUSINESSES,
+//         payload
+//     }
+// }
+
+
+// export const getBusinesses = (searchFormState) => {
+//     return dispatch => {
+//         fetch("http://localhost:9000/search", {
+//             method: 'POST', // or 'PUT'
+//             body: JSON.stringify(searchFormState), // data can be `string` or {object}!
+//             headers:{
+//                 'Content-Type': 'application/json'
+//             }
+//         })
+//         .then(res => res.json())
+//         .then(businesses => {
+//             dispatch(setBusinesses(businesses))
+//         });
+//     }
+// }
+
+
+// const setBusiness = payload => {
+//     return {
+//         type: types.GET_BUSINESS,
+//         payload
+//     }
+// }
+
+// export const getBusiness = (businessData) => {
+//     return dispatch => {
+//         fetch(`http://localhost:9000/businesses/${businessData}`, {
+//             method: 'POST', // or 'PUT'
+//             // body: JSON({businessId: businessData}),
+//             headers:{
+//                 // 'Authorization': 'Bearer rt6lfhSBGzZuHTOkC7EMF-e9RrWl-Zh0ipViu8IDDszuGHi83nhWZNIXeo6C3YPsT5E-Y8VId77gbQ8ci-v3IrgKOfWwXapQwUTDTxboVk8CFZ9y0-3A1WZ6n90dXXYx',
+//                 'Content-Type': 'application/json'
+//             }
+//         })
+//         // .then(res => res.json())
+//         .then(res => console.log(res))
+//     }
+// }
+
+
+
+
 const setBusinesses = payload => {
     return {
         type: types.GET_BUSINESSES,
@@ -10,17 +60,18 @@ const setBusinesses = payload => {
 
 export const getBusinesses = (searchFormState) => {
     return dispatch => {
-        fetch("http://localhost:9000/search", {
-            method: 'POST', // or 'PUT'
-            body: JSON.stringify(searchFormState), // data can be `string` or {object}!
+        fetch("/", {
+            method: 'GET', // or 'PUT'
+            // body: JSON.stringify(searchFormState), // data can be `string` or {object}!
             headers:{
                 'Content-Type': 'application/json'
             }
         })
-        .then(res => res.json())
-        .then(businesses => {
-            dispatch(setBusinesses(businesses))
-        });
+        // .then(res => res.json())
+        .then(res => console.log(res))
+        // .then(businesses => {
+        //     dispatch(setBusinesses(businesses))
+        // });
     }
 }
 
@@ -46,7 +97,4 @@ export const getBusiness = (businessData) => {
         .then(res => console.log(res))
     }
 }
-
-
-
 
